74c874023abfafcffb190a911a683f6806fd701b
liwangwnag123 <2317790024@qq.com>
2016-09-12 15:41:36 +0800

    项目初始化

---
+++ ReferenceMessage/ReferenceMessage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReferenceMessage
+//
+//  Created by 王力 on 16/9/12.
+//  Copyright © 2016年 王力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReferenceMessage/ReferenceMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReferenceMessage
+//
+//  Created by 王力 on 16/9/12.
+//  Copyright © 2016年 王力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
